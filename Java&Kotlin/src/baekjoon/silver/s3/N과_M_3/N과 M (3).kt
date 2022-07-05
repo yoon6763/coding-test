3f1bb9a13fa29a44365f0dfebe9b4ad9405d8ea2
Yoon <yoon6763@naver.com>
2022-07-05 13:55:12 +0900

    22.07.05

--- Java&Kotlin/src/baekjoon/silver/s3/N과_M_3/N과 M (3).kt
+++ Java&Kotlin/src/baekjoon/silver/s3/N과_M_3/N과 M (3).kt
@@ -1,10 +1,11 @@
-package baekjoon.silver.s3
+package baekjoon.silver.s3.N과_M_3
 
 lateinit var visited: Array<Boolean>
 lateinit var arr: Array<Int>
 
 var n = 0
 var m = 0
+val sb = StringBuilder()
 
 fun main() {
     val nm = readLine()!!.split(" ")
@@ -15,24 +16,19 @@ fun main() {
     visited = Array(n + 1) { false }
 
     dfs(1, 0, "")
+    print(sb.toString())
 }
 
 fun dfs(idx: Int, len: Int, str: String) {
     if (len == m) {
-        println(str)
+        sb.append(str).append("\n")
         return
     }
 
-    for (i in idx..n) {
-        if (!visited[i]) {
-            visited[i] = true
-
-            if (len == 0)
-                dfs(i, 1, arr[i].toString())
-            else
-                dfs(i, len + 1, "$str ${arr[i]}")
-
-            visited[i] = false
-        }
+    for (i in 1..n) {
+        if (len == 0)
+            dfs(i, 1, arr[i].toString())
+        else
+            dfs(i, len + 1, "$str ${arr[i]}")
     }
 }
