43b2cf305ea4c51bf07b04079fcdd2c56b8604eb
Yoon <yoon6763@naver.com>
2022-07-06 13:44:36 +0900

    22.07.06

--- src/baekjoon/silver/s1/RGB거리.kt
+++ src/baekjoon/silver/s1/RGB거리.kt
@@ -3,34 +3,29 @@ package baekjoon.silver.s1
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
+import kotlin.math.min
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
 
-    val dp = Array(n + 1) { 0 }
+    val dp = Array(n + 1) { arrayOf(0, 0, 0) } // 이전 RGB를 담음
     val rgb = Array(n + 1) { arrayOf(0, 0, 0) }
 
     for (i in 1..n) {
         val st = StringTokenizer(br.readLine(), " ")
-        repeat(3) { it2 ->
-            rgb[i][it2] = st.nextToken().toInt()
-        }
+        repeat(3) { it2 -> rgb[i][it2] = st.nextToken().toInt() }
     }
-    var result = rgb[1].minOrNull()!!
-    dp[1] = rgb[1].indexOf(result)
+
+    dp[1][0] = rgb[1][0]
+    dp[1][1] = rgb[1][1]
+    dp[1][2] = rgb[1][2]
 
     for (i in 2..n) {
-        var temp = Int.MAX_VALUE
-
-        for(j in 0 .. 2) {
-            if(dp[i-1] != j && temp > rgb[i][j]) {
-                temp = rgb[i][j]
-                dp[i] = j
-                result += rgb[i][j]
-            }
-        }
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0]
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1]
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2]
     }
 
-    println(result)
+    println(dp[n].minOrNull()!!)
 }
