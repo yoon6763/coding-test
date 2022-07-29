25b7b8d45aefa1bc1082e75fa873fe6f8d092e0d
Yoon <yoon6763@naver.com>
2022-07-29 10:19:02 +0900

    22.07.29

--- src/Practice.kt
+++ src/Practice.kt
@@ -1,7 +1,24 @@
-
 fun main() {
 
-    fun test() {
-        println("asdf")
+    val arr1 = Array(3) { Array(3) { 0 } }
+    val arr2 = Array(3) { Array(3) { 0 } }
+
+    for(i in 0 until 3) {
+        arr2[i] = arr1[i].copyOf()
     }
+
+    arr2[2][2] = 3
+    arr2[1][1] = 23
+    arr1[2][0] = -3
+
+    arr1.forEach {
+        println(it.contentToString())
+    }
+
+    println()
+
+    arr2.forEach {
+        println(it.contentToString())
+    }
+
 }
