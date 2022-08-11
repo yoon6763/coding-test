501c16d5085bc2b68f3123e9f8c57f405021aa12
uknow <yoon6763@naver.com>
2022-08-11 21:14:21 +0900

    22.08.11

--- src/baekjoon/gold/g4/미세먼지 안녕!.kt
+++ src/baekjoon/gold/g4/미세먼지 안녕!.kt
@@ -15,99 +15,86 @@ fun main() {
     val c = rct[1].toInt()
     val t = rct[2].toInt()
 
-    val map = Array(r) { Array(c) { 0 } }
-    val copyMap = Array(r) { Array(c) { 0 } }
+    var map = Array(r + 1) { Array(c + 1) { 0 } }
     val airCleaner = Array(2) { 0 } // 열은 어차피 0으로 고정, 행 값만 받음
 
     var cleanerCnt = 0
-    repeat(r) { x ->
+    for (x in 1..r) {
         val st = StringTokenizer(br.readLine(), " ")
-        repeat(c) { y ->
+        for (y in 1..c) {
             map[x][y] = st.nextToken().toInt()
 
             if (map[x][y] == -1) airCleaner[cleanerCnt++] = x
         }
     }
 
-    for (case in 0 until t) {
 
-        // 맵 복사
-        repeat(r) { x ->
-            repeat(c) { y ->
-                copyMap[x][y] = map[x][y]
-            }
-        }
+    for (case in 0 until t) {
+        val copyMap = Array(r+1) { Array(c+1) {0} }
 
         // 미세먼지 확산
-        repeat(r) { x ->
-            repeat(c) { y ->
-                if (map[x][y] > 0) {
-                    var spreadCnt = 0
-
-                    for (i in 0 until 4) {
-                        val nx = x + dx[i]
-                        val ny = y + dy[i]
-
-                        if (nx !in 0 until r || ny !in 0 until c || map[nx][ny] == -1) continue
-
-                        copyMap[nx][ny] += map[nx][ny] / 5
-                        spreadCnt++
-                    }
-                    copyMap[x][y] -= map[x][y] / 5 * spreadCnt
+        for (x in 1..r) {
+            for (y in 1..c) {
+                if (map[x][y] <= 0) {
+                    if (map[x][y] == -1) copyMap[x][y] = map[x][y]
+                    continue
                 }
+
+                var spreadCnt = 0
+
+                for (i in 0 until 4) {
+                    val nx = x + dx[i]
+                    val ny = y + dy[i]
+
+                    if (nx !in 1 .. r || ny !in 1 .. c || map[nx][ny] == -1) continue
+
+                    copyMap[nx][ny] += map[x][y] / 5
+                    spreadCnt++
+                }
+                copyMap[x][y] += map[x][y] - (map[x][y] / 5 * spreadCnt)
             }
         }
-
-
-        // 공기청정기 가동 (상부)
-        for (i in airCleaner[0] - 1 downTo 1) {
-            copyMap[i][0] = copyMap[i - 1][0]
-        }
-
-        for (i in 0 until c - 1) {
-            copyMap[0][i] = copyMap[0][i + 1]
-        }
-
-        for (i in 0 until airCleaner[0] - 1) {
-            copyMap[i][c - 1] = copyMap[i + 1][c - 1]
-        }
-
-        for (i in c - 1 downTo 1) {
-            copyMap[airCleaner[0]][i] = copyMap[airCleaner[0]][i - 1]
-        }
-
-
-        // 공기청정기 가동 (하부)
-        for (i in airCleaner[1] + 1 until r - 1) {
-            copyMap[i][0] = copyMap[i + 1][0]
-        }
-        for (i in 0 until c - 1) {
-            copyMap[0][i] = copyMap[0][i + 1]
-        }
-        for (i in r - 1 downTo airCleaner[1]) {
-            copyMap[i][c - 1] = copyMap[i - 1][c - 1]
-        }
-        for (i in c - 1 downTo 1) {
-            copyMap[airCleaner[1]][i] = copyMap[airCleaner[1]][i - 1]
-        }
-
-        copyMap[1][airCleaner[0]] = 0
-        copyMap[1][airCleaner[1]] = 0
-
 
         // 맵 복사
-        repeat(r) { x ->
-            repeat(c) { y ->
-                map[x][y] = copyMap[x][y]
-            }
+        map = copyMap
+
+        for (i in airCleaner[0] - 1 downTo 2) {
+            map[i][1] = map[i - 1][1]
         }
+
+        for (i in 1 until c) {
+            map[1][i] = map[1][i + 1]
+        }
+
+        for (i in 1 until airCleaner[0]) {
+            map[i][c] = map[i + 1][c]
+        }
+
+        for (i in c downTo 3) {
+            map[airCleaner[0]][i] = map[airCleaner[0]][i - 1]
+        }
+        map[airCleaner[0]][2] = 0
+
+        for (i in airCleaner[1] + 1 until r) {
+            map[i][1] = map[i + 1][1]
+        }
+        for (i in 0 until c) {
+            map[r][i] = map[r][i + 1]
+        }
+        for (i in r downTo airCleaner[1] + 1) {
+            map[i][c] = map[i - 1][c]
+        }
+        for (i in c downTo 3) {
+            map[airCleaner[1]][i] = map[airCleaner[1]][i - 1]
+        }
+        map[airCleaner[1]][2] = 0
     }
 
     var result = 0
 
-    repeat(r) { x ->
-        repeat(c) { y ->
-            if (map[x][y] != -1) result += map[x][y]
+    for (x in 1..r) {
+        for (y in 1..c) {
+            if (map[x][y] > 0) result += map[x][y]
         }
     }
 
