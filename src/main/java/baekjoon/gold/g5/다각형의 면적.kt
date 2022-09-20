8fe70df9b6fa6c84ce525005f247f3cbaf3b59fc
DESKTOP-TFUUGQ7\8_915_차준혁 <yoon6763@naver.com>
2022-09-20 16:59:41 +0900

    22.09.20 Part 2

--- src/main/java/baekjoon/gold/g5/다각형의 면적.kt
+++ src/main/java/baekjoon/gold/g5/다각형의 면적.kt
@@ -4,8 +4,9 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import kotlin.math.abs
 import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
-data class Point(var x: Int, var y: Int)
+data class Point(var x: Long, var y: Long)
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
@@ -14,7 +15,7 @@ fun main() {
     val points = Array(n) { Point(0, 0) }
 
     repeat(n) {
-        val line = br.readLine().split(" ").map { it.toInt() }
+        val line = br.readLine().split(" ").map { it.toLong() }
         points[it].x = line[0]
         points[it].y = line[1]
     }
@@ -25,9 +26,9 @@ fun main() {
         result += area(points[0], points[i], points[i + 1])
     }
 
-    println((result * 100).roundToInt() / 100.toDouble())
+    println("${String.format("%.1f", abs(result / 2.0))}")
 }
 
-fun area(p1: Point, p2: Point, p3: Point): Double {
-    return abs(((p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p1.x * p3.y + p2.x * p1.y + p3.x * p2.y)).toDouble() / 2)
+fun area(p1: Point, p2: Point, p3: Point): Long {
+    return ((p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p1.x * p3.y + p2.x * p1.y + p3.x * p2.y))
 }
