95cd13a94bed7661982b559f6c687d2fbf904d12
uknow <yoon6763@naver.com>
2022-10-02 01:19:14 +0900

    C, C++ 추가

---
+++ C/CLangCodingTest/CLangCodingTest/A+B - 3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	int i;
+
+	cin >> n;
+
+	int a, b;
+
+	for (i = 0; i < n; i++)
+	{
+		cin >> a;
+		cin >> b;
+
+		cout << a + b << endl;
+	}
+
+}
