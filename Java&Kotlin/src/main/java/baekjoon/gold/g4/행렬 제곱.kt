a9ff7ad3b183e828d038f2686fbb8e875e02730c
915_랩실컴 <yoon6763@naver.com>
2022-10-26 13:48:29 +0900

    22.10.26 - 2

--- Java&Kotlin/src/main/java/baekjoon/gold/g4/행렬 제곱.kt
+++ Java&Kotlin/src/main/java/baekjoon/gold/g4/행렬 제곱.kt
@@ -4,22 +4,60 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
 
+var n = 0
+var b = 0L
+lateinit var arr: Array<Array<Int>>
+lateinit var answer: Array<Array<Int>>
+
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
 
     val nb = br.readLine().split(" ")
-    val n = nb[0].toInt()
-    val b = nb[1].toInt() // 제곱
+    n = nb[0].toInt()
+    b = nb[1].toLong() // 제곱
 
-    val arr = Array(n) { Array(n) { 0 } }
+    arr = Array(n) { Array(n) { 0 } }
+    answer = Array(n) { Array(n) { 0 } }
 
     repeat(n) { i ->
         val st = StringTokenizer(br.readLine())
         repeat(n) { j ->
             arr[i][j] = st.nextToken().toInt()
         }
+        answer[i][i] = 1
     }
 
 
+    while (b > 0) {
+        if (b % 2 == 1L) {
+            answer = matrixMultiply(answer, arr)
+        }
+        arr = matrixMultiply(arr, arr)
+        b /= 2
+    }
 
+
+    val sb = StringBuilder()
+    answer.forEach { it ->
+        it.forEach { it2 ->
+            sb.append("$it2 ")
+        }
+        sb.append("\n")
+    }
+    print(sb)
+}
+
+
+
+fun matrixMultiply(m1: Array<Array<Int>>, m2: Array<Array<Int>>): Array<Array<Int>> {
+    val resultArr = Array(n) { Array(n) { 0 } }
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            for (k in 0 until n) {
+                resultArr[i][j] += m1[i][k] * m2[k][j]
+                resultArr[i][j] %= 1000
+            }
+        }
+    }
+    return resultArr
 }
