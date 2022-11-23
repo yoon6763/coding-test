d0721050c90ab232872ed9ee32fa1a2e37378f84
915_랩실컴 <yoon6763@naver.com>
2022-11-23 13:09:54 +0900

    백준크롤링 파일 디폴트로 주석처리

--- Java&Kotlin/src/main/java/백준크롤링.kt
+++ Java&Kotlin/src/main/java/백준크롤링.kt
@@ -1,44 +1,44 @@
-import org.jsoup.Jsoup
-
-fun main() {
-    println("백준 문제 크롤링 중 ... ")
-    val url = "https://www.acmicpc.net/user/yoon6763"
-    val conn = Jsoup.connect(url)
-    val html = conn.get()
-
-    // 맞은 문제, 시도했지만 맞지 못한 문제, 맞은 번외 문제
-    val classTag = html.getElementsByClass("problem-list")
-
-    val problemsTags = classTag[0].getElementsByTag("a")
-    val bjProblems = problemsTags.map { it.text().toInt() }
-    println("백준 문제 크롤링 완료\n")
-
-    println("티스토리 탐색중 ...")
-
-    val blogProblems = ArrayList<Int>()
-
-    for (i in 1..100) {
-        try {
-            println("티스토리 탐색 - $i")
-            val tistoryUrl = "https://uknowblog.tistory.com/$i"
-            val tistoryConn = Jsoup.connect(tistoryUrl)
-            val tistoryHtml = tistoryConn.get()
-
-            val title = tistoryHtml.title()
-            if (title.startsWith("[백준 ")) {
-                val problem = title.split("]")[0].filter { it.isDigit() }.toInt()
-                blogProblems.add(problem)
-            }
-        } catch (e: Exception) {
-            println("오류페이지 => $i")
-        }
-    }
-
-    println("\n\n포스팅하지 않은 문제")
-    bjProblems.forEach {
-        if (it !in blogProblems) {
-            println(it)
-        }
-    }
-    println("총 풀은 문제 : ${bjProblems.size}\t총 포스팅한 문제 : ${blogProblems.size}\t포스팅하지 않은 문제 : ${bjProblems.size - blogProblems.size}")
-}
+//import org.jsoup.Jsoup
+//
+//fun main() {
+//    println("백준 문제 크롤링 중 ... ")
+//    val url = "https://www.acmicpc.net/user/yoon6763"
+//    val conn = Jsoup.connect(url)
+//    val html = conn.get()
+//
+//    // 맞은 문제, 시도했지만 맞지 못한 문제, 맞은 번외 문제
+//    val classTag = html.getElementsByClass("problem-list")
+//
+//    val problemsTags = classTag[0].getElementsByTag("a")
+//    val bjProblems = problemsTags.map { it.text().toInt() }
+//    println("백준 문제 크롤링 완료\n")
+//
+//    println("티스토리 탐색중 ...")
+//
+//    val blogProblems = ArrayList<Int>()
+//
+//    for (i in 1..100) {
+//        try {
+//            println("티스토리 탐색 - $i")
+//            val tistoryUrl = "https://uknowblog.tistory.com/$i"
+//            val tistoryConn = Jsoup.connect(tistoryUrl)
+//            val tistoryHtml = tistoryConn.get()
+//
+//            val title = tistoryHtml.title()
+//            if (title.startsWith("[백준 ")) {
+//                val problem = title.split("]")[0].filter { it.isDigit() }.toInt()
+//                blogProblems.add(problem)
+//            }
+//        } catch (e: Exception) {
+//            println("오류페이지 => $i")
+//        }
+//    }
+//
+//    println("\n\n포스팅하지 않은 문제")
+//    bjProblems.forEach {
+//        if (it !in blogProblems) {
+//            println(it)
+//        }
+//    }
+//    println("총 풀은 문제 : ${bjProblems.size}\t총 포스팅한 문제 : ${blogProblems.size}\t포스팅하지 않은 문제 : ${bjProblems.size - blogProblems.size}")
+//}
