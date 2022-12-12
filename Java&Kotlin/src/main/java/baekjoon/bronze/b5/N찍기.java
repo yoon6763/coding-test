0743bec7a20c6beed8b4935d84bf48fe9a664f54
uknow <yoon6763@naver.com>
2022-12-13 00:32:13 +0900

    일부 코드 주석 추가

--- Java&Kotlin/src/main/java/baekjoon/bronze/b5/N찍기.java
+++ Java&Kotlin/src/main/java/baekjoon/bronze/b5/N찍기.java
@@ -3,19 +3,13 @@ package baekjoon.bronze.b5;
 import java.util.Scanner;
 
 public class N찍기 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
 
-        int i;
-
-        for(i=1;i<=a;i++)
-        {
+        for (int i = 1; i <= a; i++) {
             System.out.println(i);
         }
-
     }
-
 }
