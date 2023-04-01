1c142d7b58672441defd4ea98286865facc6de34
uknow-home <yoon6763@naver.com>
2023-04-01 11:07:50 +0900

    23.04.01

--- Kotlin-Java/src/main/kotlin/baekjoon/gold/g5/동전 1.kt
+++ Kotlin-Java/src/main/kotlin/baekjoon/gold/g5/동전 1.kt
@@ -2,9 +2,11 @@ package baekjoon.gold.g5
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import kotlin.math.min
 
 
 fun main(): Unit = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val MAX = 1000000
     val (n, k) = readLine().split(" ").map { it.toInt() }
 
     val coins = Array(n) { 0 }
@@ -12,14 +14,15 @@ fun main(): Unit = with(BufferedReader(InputStreamReader(System.`in`))) {
         coins[it] = readLine().toInt()
     }
 
-    val dp = Array(k + 1) { 0 }
-    dp[0] = 1
+    val dp = Array(k + 1) { MAX }
+    dp[0] = 0
 
-    for (coin in coins) {
-        for (i in coin..k) {
-            dp[i] += dp[i - coin]
+    for (i in 1..k) {
+        for (coin in coins) {
+            if (i - coin >= 0) {
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+            }
         }
     }
-
-    println(dp[k])
+    println(if (dp[k] == MAX) -1 else dp[k])
 }
