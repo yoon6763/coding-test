c65f376498569a3aaf7d65a79e58ebba97caed37
uknow-home <yoon6763@naver.com>
2023-06-17 15:56:10 +0900

    23.06.17 - 2

--- kotlin-java-codingtest/src/main/kotlin/baekjoon/bronze/b3/공.kt
+++ kotlin-java-codingtest/src/main/kotlin/baekjoon/bronze/b3/공.kt
@@ -1,15 +1,15 @@
 package baekjoon.bronze.b3
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val goal = BooleanArray(3)
-    goal[0] = true
+    val cup = BooleanArray(3)
+    cup[0] = true
 
     repeat(readLine().toInt()) {
         val (a, b) = readLine().split(" ").map { it.toInt() - 1 }
-        val temp = goal[a]
-        goal[a] = goal[b]
-        goal[b] = temp
+        val temp = cup[a]
+        cup[a] = cup[b]
+        cup[b] = temp
     }
 
-    println(goal.indexOf(true) + 1)
+    println(cup.indexOf(true) + 1)
 }
