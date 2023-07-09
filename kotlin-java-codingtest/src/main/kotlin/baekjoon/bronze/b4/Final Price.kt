d4b5c23ed2ec872c6a415cdbfa27424db1c410b0
uknow-home <yoon6763@naver.com>
2023-07-09 19:09:58 +0900

    23.07.09 - 2

---
+++ kotlin-java-codingtest/src/main/kotlin/baekjoon/bronze/b4/Final Price.kt
@@ -0,0 +1,3 @@
+package baekjoon.bronze.b4
+
+fun main()=println(Array(readln().toInt()){readln().toInt()}.sum())
---
+++ kotlin-java-codingtest/src/main/kotlin/baekjoon/silver/s1/하노이 탑 이동 순서.kt
@@ -0,0 +1,23 @@
+package baekjoon.silver.s1
+
+import java.lang.StringBuilder
+
+val sb = StringBuilder()
+
+fun main() {
+    val n = readln().toInt()
+    sb.append("${(1 shl n) - 1}\n")
+    hanoi(n, 1, 2, 3)
+    print(sb)
+}
+
+fun hanoi(n: Int, from: Int, mid: Int, to: Int) {
+    if (n == 1) {
+        sb.append("$from $to\n")
+        return
+    }
+
+    hanoi(n - 1, from, to, mid)
+    sb.append("$from $to\n")
+    hanoi(n - 1, mid, from, to)
+}
