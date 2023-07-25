9de3d5b80524c7d3ace50b62f0a86e7a5b8be5b6
uknow-home <yoon6763@naver.com>
2023-07-25 23:36:39 +0900

    23.07.25 - 2

--- kotlin-java-codingtest/src/main/kotlin/baekjoon/bronze/b5/팩토리얼 3.kt
+++
@@ -1,8 +0,0 @@
-package baekjoon.bronze.b5
-
-import java.math.BigInteger
-fun main() {
-    var result = BigInteger.ONE
-    for (i in 2..readln().toInt()) result *= i.toBigInteger()
-    println(result)
-}
