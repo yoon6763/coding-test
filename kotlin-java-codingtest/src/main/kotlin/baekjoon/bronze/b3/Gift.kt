ef286398004e5401ee6c0538ac13dce1beecf277
uknow-home <yoon6763@naver.com>
2023-08-01 00:43:48 +0900

    23.07.31 - 3

--- kotlin-java-codingtest/src/main/kotlin/baekjoon/bronze/b3/Gift.kt
+++ kotlin-java-codingtest/src/main/kotlin/baekjoon/bronze/b3/Gift.kt
@@ -4,6 +4,6 @@ fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
     val arr = Array(n) { readln().toInt() }
     var cnt = 0
-    for (i in 1 until n) if (arr[i] - arr[i - 1] >= m) cnt++
+    for (i in 1 until n) if (arr[i - 1] - arr[i] >= m) cnt++
     println(cnt)
 }
