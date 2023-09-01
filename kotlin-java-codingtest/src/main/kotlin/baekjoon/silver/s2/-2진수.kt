5e8bf8e370999a932e836fd5bffec4b2b42c44fc
uknow-home <yoon6763@naver.com>
2023-09-01 13:19:49 +0900

    23.09.01

---
+++ kotlin-java-codingtest/src/main/kotlin/baekjoon/silver/s2/-2진수.kt
@@ -0,0 +1,17 @@
+package baekjoon.silver.s2
+
+import kotlin.math.abs
+import kotlin.math.ceil
+
+fun main() {
+    var n = readln().toInt()
+    val sb = StringBuilder()
+
+    while (n != 1) {
+        sb.append(abs(n % -2))
+        n = ceil(n.toDouble() / -2).toInt()
+    }
+    sb.append(n)
+
+    println(sb.reverse())
+}
