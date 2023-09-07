d0e0244a2a24e7c667b4cb5f88f2909da1e3ad68
uknow-mac <yoon6763@naver.com>
2023-09-07 17:42:35 +0900

    23.09.07 - 2

--- kotlin-java-codingtest/src/main/kotlin/groomthonchallenge/p19_대체경로/Main.kt
+++ kotlin-java-codingtest/src/main/kotlin/groomthonchallenge/p19_대체경로/Main.kt
@@ -1,60 +1,70 @@
 package groomthonchallenge.p19_대체경로
 
-import java.util.StringTokenizer
-
-var n = 0
-var m = 0
-var s = 0
-var e = 0
-lateinit var connect: Array<Array<Boolean>>
-lateinit var visited: BooleanArray
-lateinit var minDistanceWhenCityDisable: IntArray
+import java.util.HashSet
+import java.util.LinkedList
+import java.util.Queue
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val nmse = StringTokenizer(readLine())
-    n = nmse.nextToken().toInt()
-    m = nmse.nextToken().toInt()
-    s = nmse.nextToken().toInt()
-    e = nmse.nextToken().toInt()
+    val (n, m, start, end) = readLine().split(" ").map(String::toInt)
 
-    connect = Array(n + 1) { Array(n + 1) { false } }
-    minDistanceWhenCityDisable = IntArray(n + 1) { Int.MAX_VALUE }
+    val connect = Array(n + 1) { ArrayList<Int>() }
+    val minDistanceWhenCityBlocked = IntArray(n + 1) { Int.MAX_VALUE }
+
     repeat(m) {
-        val st = StringTokenizer(readLine())
-        val (a, b) = st.nextToken().toInt() to st.nextToken().toInt()
-        connect[a][b] = true
-        connect[b][a] = true
+        val (a, b) = readLine().split(" ").map(String::toInt)
+        connect[a].add(b)
+        connect[b].add(a)
     }
 
-    visited = BooleanArray(n + 1) { false }
-    visited[s] = true
-    dfs(s, 1)
+    val visited = IntArray(n + 1) { 0 }
+    val preNode = IntArray(n + 1) { -1 }
 
+    // 0번 도시가 공사중 -> 아무 도시도 공사중이지 않은 상태를 의미
+    for (blockedCity in 0..n) {
+        visited.fill(0)
+        preNode.fill(-1)
+
+        visited[start] = 1
+
+        val q = LinkedList<Int>() as Queue<Int>
+        q.offer(start)
+
+        bfs@ while (q.isNotEmpty()) {
+            val currentCity = q.poll()
+
+            for (i in 0 until connect[currentCity].size) {
+                val nextCity = connect[currentCity][i]
+
+                if (visited[nextCity] != 0 || nextCity == blockedCity) continue
+
+                visited[nextCity] = visited[currentCity] + 1
+                preNode[nextCity] = currentCity
+                q.offer(nextCity)
+
+                if (nextCity == end) {
+                    val visitedCity = HashSet<Int>()
+                    var pre = end
+
+                    while (pre != -1) {
+                        visitedCity.add(pre)
+                        pre = preNode[pre]
+                    }
+
+                    for (j in 1..n) {
+                        if (j in visitedCity) continue
+                        minDistanceWhenCityBlocked[j] = minOf(minDistanceWhenCityBlocked[j], visited[end])
+                    }
+
+                    break@bfs
+                }
+            }
+        }
+    }
 
     val sb = StringBuilder()
     for (i in 1..n) {
-        if (minDistanceWhenCityDisable[i] == Int.MAX_VALUE) sb.append(-1).append("\n")
-        else sb.append(minDistanceWhenCityDisable[i]).append("\n")
+        if (minDistanceWhenCityBlocked[i] == Int.MAX_VALUE) sb.append(-1).append("\n")
+        else sb.append(minDistanceWhenCityBlocked[i]).append("\n")
     }
-
     print(sb)
-}
-
-fun dfs(cur: Int, depth: Int) {
-    if (cur == e) {
-        for (disableCity in 1..n) {
-            if (visited[disableCity]) continue
-            minDistanceWhenCityDisable[disableCity] = minDistanceWhenCityDisable[disableCity].coerceAtMost(depth)
-        }
-
-        return
-    }
-
-    for (next in 1..n) {
-        if (visited[next] || !connect[cur][next]) continue
-        visited[next] = true
-
-        dfs(next, depth + 1)
-        visited[next] = false
-    }
 }
