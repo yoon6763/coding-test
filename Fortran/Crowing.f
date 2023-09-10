34460c06d52f03844f9bb3c901d68c493d7be9ce
uknow-home <yoon6763@naver.com>
2023-09-11 02:36:18 +0900

    기타 언어 추가

---
+++ Fortran/Crowing.f
@@ -0,0 +1,8 @@
+program print_ascii
+  print '(a)', ",~"
+  print '(a)', "( 9> //)"
+  print '(a)', " )(_///"
+  print '(a)', "( \_>/"
+  print '(a)', " \__/"
+  print '(a)', "  ^^"
+end program print_ascii
