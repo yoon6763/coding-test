a2d640abb08399eb804041aa6f593f1fa6a4603c
uknow-home <yoon6763@naver.com>
2023-09-18 13:09:36 +0900

    23.09.18

--- kotlin-java-codingtest/src/main/kotlin/Count.kt
+++ kotlin-java-codingtest/src/main/kotlin/Count.kt
@@ -1,28 +1,72 @@
 import java.io.File
 
-var cnt = 0
-
-fun main() {
-    val defaultPath = System.getProperty("user.dir")
-    val codePath = "${defaultPath}/src"
-
-
-    dfs(codePath, 0)
-    println(cnt)
+enum class Type {
+    PROGRAMMERS, BAEKJOON
 }
 
-fun dfs(path: String, deep: Int) {
+data class Problem(val problemName: String, val language: String, val level: String, val type: Type)
+
+var count = HashMap<String, Int>()
+val extension = hashMapOf(
+    "a68" to "algol68",
+    "c" to "C",
+    "cpp" to "C++",
+    "java" to "Java",
+    "kt" to "Kotlin",
+    "f95" to "Fortran",
+    "bas" to "FreeBASIC",
+    "gs" to "GolfScript",
+    "js" to "JavaScript",
+    "py" to "Python",
+    "rb" to "Ruby",
+    "sql" to "SQL",
+    "vb" to "Visual Basic",
+)
+
+fun main() {
+    val javaKotlinPath = System.getProperty("user.dir")
+    val defaultPath = File(javaKotlinPath).parent
+
+    countSolvedProblems(defaultPath, 0)
+    updateReadMe(defaultPath)
+}
+
+fun updateReadMe(defaultFile: String) {
+    val readMe = File("$defaultFile/README.md")
+    val content = StringBuilder()
+
+    val bufferedReader = readMe.bufferedReader()
+
+    while (true) {
+        val line = bufferedReader.readLine() ?: break
+        content.appendLine(line)
+        if (line.contains("## Solved Problems")) break
+    }
+
+    count.toList().sortedByDescending { it.second }.forEach {
+        content.appendLine("- ${extension[it.first]}: ${it.second} 문제")
+    }
+
+
+    readMe.writeText(content.toString())
+}
+
+fun countSolvedProblems(path: String, deep: Int) {
     val folder = File(path)
+
+    println(folder.path)
 
     folder.list()?.forEach {
         repeat(deep) { print("\t") }
-        println(it)
 
-        if ((folder.path.contains("programmers") || folder.path.contains("baekjoon"))
-            && (it.endsWith(".kt") || it.endsWith(".java"))
-        )
-            cnt++
+        if (folder.path.contains("programmers") || folder.path.contains("baekjoon")) {
+            val ext = it.split(".").last()
+            if (ext in extension.map { it.key }) {
+                count[ext] = count.getOrDefault(ext, 0) + 1
+                println(it)
+            }
+        }
 
-        dfs("${folder.path}/$it", deep + 1)
+        countSolvedProblems("${folder.path}/$it", deep + 1)
     }
 }
