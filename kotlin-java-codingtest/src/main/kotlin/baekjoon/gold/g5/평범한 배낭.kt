3f365a509e77630e4d763c490a4a07c94b02523d
uknow-mac <yoon6763@naver.com>
2023-10-24 15:45:41 +0900

    23.10.24 - 2

--- kotlin-java-codingtest/src/main/kotlin/baekjoon/gold/g5/평범한 배낭.kt
+++ kotlin-java-codingtest/src/main/kotlin/baekjoon/gold/g5/평범한 배낭.kt
@@ -1,31 +1,24 @@
 package baekjoon.gold.g5
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import kotlin.math.max
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, k) = readLine().split(" ").map(String::toInt)
+    val arr = Array(n + 1) { Array(2) { 0 } } // 0 - 무게, 1 - 가치
 
-
-fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val nk = br.readLine().split(" ")
-    val n = nk[0].toInt()
-    val k = nk[1].toInt() // 버틸 수 있는 무게
-    val dp = Array(n + 1) { Array(k + 1) { 0 } }
-
-    val objs = Array(n + 1) { Array(2) { 0 } } // 0 - 무게, 1 - 가치
-
-    repeat(n) {
-        val line = br.readLine().split(" ")
-        objs[it + 1][0] = line[0].toInt()
-        objs[it + 1][1] = line[1].toInt()
+    for (i in 1 until n) {
+        val (w, v) = readLine().split(" ").map(String::toInt)
+        arr[i][0] = w
+        arr[i][1] = v
     }
+
+    val dp = Array(n + 1) { Array(k + 1) { 0 } }
 
     for (i in 1..n) {
         for (j in 1..k) {
-            if (j - objs[i][0] >= 0)
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - objs[i][0]] + objs[i][1])
-            else
+            if (j - arr[i][0] >= 0) {
+                dp[i][j] = maxOf(dp[i - 1][j], dp[i - 1][j - arr[i][0]] + arr[i][1])
+            } else {
                 dp[i][j] = dp[i - 1][j]
+            }
         }
     }
 
