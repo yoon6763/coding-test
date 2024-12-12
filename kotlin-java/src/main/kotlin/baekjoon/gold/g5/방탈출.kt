c3d07adb9bccb43aa6c935cde6fe7583478ed856
전윤호 Uknow <yoon6763@naver.com>
2024-12-12 23:58:18 +0900

    24.12.12 - 2

--- kotlin-java/src/main/kotlin/baekjoon/gold/g5/방탈출.kt
+++ kotlin-java/src/main/kotlin/baekjoon/gold/g5/방탈출.kt
@@ -5,13 +5,7 @@ import java.util.*
 fun main() = with(System.`in`.bufferedReader()) {
     data class Node(val x: Int, val y: Int)
     val (n, m) = readLine().split(" ").map { it.toInt() }
-
     val board = Array(n) { readLine().split(" ").map { it.toInt() }.toIntArray() }
-    if (board.all { it.all { it == 0 } }) {
-        println(0)
-        return
-    }
-
     val dx = intArrayOf(-1, 1, 0, 0)
     val dy = intArrayOf(0, 0, -1, 1)
 
@@ -20,32 +14,33 @@ fun main() = with(System.`in`.bufferedReader()) {
 
     repeat(n) { i ->
         repeat(m) { j ->
-            if (board[i][j] != 0) {
-                val visited = Array(n) { IntArray(m) { 0 } }
+            if (board[i][j] == 0) return@repeat
+            val visited = Array(n) { IntArray(m) { 0 } }
+            val q = LinkedList<Node>() as Queue<Node>
+            q.add(Node(i, j))
+            visited[i][j] = 1
 
-                val q = LinkedList<Node>() as Queue<Node>
-                q.add(Node(i, j))
-                visited[i][j] = 1
+            while (q.isNotEmpty()) {
+                val (x, y) = q.poll()
 
-                while (q.isNotEmpty()) {
-                    val (x, y) = q.poll()
+                for (k in 0..<4) {
+                    val nx = x + dx[k]
+                    val ny = y + dy[k]
 
-                    for (k in 0..<4) {
-                        val nx = x + dx[k]
-                        val ny = y + dy[k]
+                    if (nx !in 0..<n || ny !in 0..<m || board[nx][ny] == 0 || visited[nx][ny] != 0) continue
 
-                        if (nx !in 0..<n || ny !in 0..<m || board[nx][ny] == 0 || visited[nx][ny] != 0) continue
+                    visited[nx][ny] = visited[x][y] + 1
+                    q.add(Node(nx, ny))
 
-                        visited[nx][ny] = visited[x][y] + 1
-                        q.add(Node(nx, ny))
+                    if (visited[nx][ny] > maxDistance) {
+                        maxDistance = visited[nx][ny]
+                        answer = board[nx][ny] + board[i][j]
+                    }
 
-                        if (visited[nx][ny] >= maxDistance) {
-                            maxDistance = visited[nx][ny]
-                            answer = board[nx][ny] + board[i][j]
-                        }
+                    if (visited[nx][ny] == maxDistance) {
+                        answer = maxOf(answer, board[nx][ny] + board[i][j])
                     }
                 }
-
             }
         }
     }
