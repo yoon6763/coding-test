b428ab54426b60d9d676dd352c23b4295cd1325c
전윤호 Uknow <yoon6763@naver.com>
2024-12-22 23:42:13 +0900

    24.12.22

---
+++ kotlin-java/src/main/kotlin/baekjoon/bronze/b2/카드놀이.kt
@@ -0,0 +1,26 @@
+package baekjoon.bronze.b2
+
+fun main() {
+    data class A(val a: Int, val b: Int, val c: Char)
+
+    val i = { readln().split(" ").map { it.toInt() } }
+    val a = i.invoke()
+    val b = i.invoke()
+    if (a == b) {
+        println("10 10")
+        println("D")
+    } else {
+        val (aScore, bScore, flag) = (0..<10).map {
+            when {
+                a[it] > b[it] -> A(3, 0, 'A')
+                a[it] < b[it] -> A(0, 3, 'B')
+                else -> A(1, 1, 'D')
+            }
+        }.reduce { acc, score ->
+            A(acc.a * 2, acc.b * 2, if (score.c != 'D') score.c else acc.c)
+        }
+
+        println("$aScore $bScore")
+        println(if (aScore == bScore) flag else if (aScore > bScore) 'A' else 'B')
+    }
+}
