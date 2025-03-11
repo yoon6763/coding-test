53945215f9634c73a2ca4f4d14428ab7f8ac9cb4
전윤호 Uknow <yoon6763@naver.com>
2025-03-11 20:31:31 +0900

    25.03.11 - 2

--- kotlin-java/src/main/kotlin/baekjoon/bronze/b3/심준의 병역판정검사.kt
+++ kotlin-java/src/main/kotlin/baekjoon/bronze/b3/심준의 병역판정검사.kt
@@ -1,22 +1,23 @@
 package baekjoon.bronze.b3
 
 fun main() = repeat(readln().toInt()) {
-    val (cm, kg) = readln().split(" ").map { it.toDouble() }
-    val bmi = kg / (cm * cm / 10000)
+    readln().split(" ").map { it.toDouble() }.let { (cm, kg) ->
+        val bmi = kg / (cm * cm / 10000)
+        println(
+            when {
+                cm < 140.1 -> 6
+                cm < 146 -> 5
+                cm < 159 -> 4
+                cm < 161 -> if (bmi >= 16.0 && bmi < 35.0) 3 else 4
+                cm < 204 -> when {
+                    bmi >= 20.0 && bmi < 25.0 -> 1
+                    (bmi >= 18.5 && bmi < 20.0) || (bmi >= 25.0 && bmi < 30.0) -> 2
+                    (bmi >= 16.0 && bmi < 18.5) || (bmi >= 30 && bmi < 35.0) -> 3
+                    else -> 4
+                }
 
-    println(
-        when {
-            cm < 140.1 -> 6
-            cm < 146 -> 5
-            cm < 159 -> 4
-            cm < 161 -> if (bmi in 16.0..34.9) 3 else 4
-            cm < 204 -> when (bmi) {
-                in 20.0..24.9 -> 1
-                in 18.5..19.9, in 25.0..29.9 -> 2
-                in 16.0..18.4, in 30.0..34.9 -> 3
                 else -> 4
             }
-            else -> 4
-        }
-    )
+        )
+    }
 }
