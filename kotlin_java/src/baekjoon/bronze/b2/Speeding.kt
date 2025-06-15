9a7c07d2ff7e73874ba3b160198bec286c4ec823
uknow-mac <yoon6763@naver.com>
2025-06-15 13:54:48 +0900

    25.06.15

---
+++ kotlin_java/src/baekjoon/bronze/b2/Speeding.kt
@@ -0,0 +1,15 @@
+package baekjoon.bronze.b2
+
+fun main() {
+    val n = readln().toInt()
+    val photos = List(n) { readln().split(" ").map { it.toInt() } }
+    var ans = 0
+
+    for (i in 1 until n) {
+        val (t1, d1) = photos[i - 1]
+        val (t2, d2) = photos[i]
+        ans = maxOf(ans, (d2 - d1) / (t2 - t1))
+    }
+
+    println(ans)
+}
