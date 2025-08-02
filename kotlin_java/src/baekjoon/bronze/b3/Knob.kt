6f259eacb8e6e69dc426605bf7143de864bb5315
전윤호 Uknow <yoon6763@naver.com>
2025-08-02 23:35:28 +0900

    25.08.02 - 2

--- kotlin_java/src/baekjoon/bronze/b3/Knob.kt
+++ kotlin_java/src/baekjoon/bronze/b3/Knob.kt
@@ -1,24 +1,23 @@
 package baekjoon.bronze.b3
 
-fun main() {
-    val n = readln().toInt()
-    val left = IntArray(n)
-    val right = IntArray(n)
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    var cnt = 0
+    var a = 0
+    var b = 0
 
-    repeat(n) { i ->
-        val (l, r) = readln().split(" ").map { it.toInt() }
-        left[i] = l
-        right[i] = r
+    repeat(n) {
+        val (c, d) = readLine().split(" ").map { it.toInt() }
+
+        if (it > 0) {
+            if (a == c && a != 0) cnt++
+            if (b == d && b != 0) cnt++
+        }
+        if (c == d && c != 0) cnt++
+
+        a = c
+        b = d
     }
 
-    var ans = 0
-
-    for (i in 1..<n) {
-        if (left[i] != 0 && left[i] == left[i - 1] ||
-            right[i] != 0 && right[i] == right[i - 1] ||
-            left[i] != 0 && right[i] != 0 && left[i] == right[i]
-        ) ans++
-    }
-    if (n >= 1 && left[0] != 0 && right[0] != 0 && left[0] == right[0]) ans++
-    println(ans)
+    println(cnt)
 }
