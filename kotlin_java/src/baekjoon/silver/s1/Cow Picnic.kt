ae539663a15023461f54246f13955e0870ac1d68
전윤호 Uknow <yoon6763@naver.com>
2025-08-04 23:23:19 +0900

    25.08.04 - 2

--- kotlin_java/src/baekjoon/silver/s1/Cow Picnic.kt
+++ kotlin_java/src/baekjoon/silver/s1/Cow Picnic.kt
@@ -1,3 +1,32 @@
 package baekjoon.silver.s1
 
-.
+fun main() {
+    val (k, n, m) = readln().split(" ").map { it.toInt() }
+
+    val startPoints = Array(k) { readln().toInt() - 1 }
+    val graph = Array(n) { mutableListOf<Int>() }
+
+    repeat(m) {
+        val (a, b) = readln().split(" ").map { it.toInt() - 1 }
+        graph[a] += b
+    }
+
+    val visitedCount = IntArray(n)
+    val visited = BooleanArray(n)
+
+    fun dfs(cur: Int) {
+        visited[cur] = true
+        visitedCount[cur]++
+        graph[cur].forEach { next ->
+            if (visited[next]) return@forEach
+            dfs(next)
+        }
+    }
+
+    startPoints.forEach { start ->
+        visited.fill(false)
+        dfs(start)
+    }
+
+    println(visitedCount.count { it == k })
+}
