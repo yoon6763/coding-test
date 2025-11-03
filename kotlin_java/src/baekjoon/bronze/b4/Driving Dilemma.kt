9d009356bab2f7cab53ef9b3e0a44bcf920c5eab
전윤호 / uknow <yoon6763@naver.com>
2025-11-03 19:39:23 +0900

    25.11.03 - 2

--- kotlin_java/src/baekjoon/bronze/b4/Driving Dilemma.kt
+++ kotlin_java/src/baekjoon/bronze/b4/Driving Dilemma.kt
@@ -1,7 +1,7 @@
 package baekjoon.bronze.b4
 
 fun main() {
-    val (s, d, t) = readln().split(' ').map { it.toDouble() }
+    val (s, d, t) = List(3) { readln().toDouble() }
     if (s * 5280 / 3600 * t >= d) println("MADE IT")
     else println("FAILED TEST")
 }
