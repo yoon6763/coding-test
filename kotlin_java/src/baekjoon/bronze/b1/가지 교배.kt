3f190ed233687005ab415f645655f725daa371f3
전윤호 / uknow <yoon986763@gmail.com>
2025-11-13 22:31:57 +0900

    25.11.13

---
+++ kotlin_java/src/baekjoon/bronze/b1/가지 교배.kt
@@ -0,0 +1,14 @@
+fun main() {
+    readln().toInt()
+    val type = readln().split(" ")
+    val (n, m) = readln().split(" ").map { it.toInt() }
+
+    repeat(n) {
+        if (readln().split(" ").map { it.toInt() }.none { type[it - 1] == "P" }) {
+            println("W")
+            return
+        }
+    }
+
+    println("P")
+}
