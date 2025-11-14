71b8d028d8c29699f23d928df168e8e80c024765
전윤호 / uknow <yoon986763@gmail.com>
2025-11-14 21:54:35 +0900

    25.11.14

---
+++ kotlin_java/src/main/java/silver/s2/Family Tree.kt
@@ -0,0 +1,30 @@
+package silver.s2
+
+fun main() {
+    val n = readln().toInt()
+    val graph = mutableMapOf<String, MutableList<String>>()
+    val root = readln()
+    graph[root] = mutableListOf()
+
+    repeat(n - 1) {
+        val (parent, child) = readln().split(" - ")
+        graph.getOrPut(parent) { mutableListOf() }.add(child)
+    }
+
+    var ans = 1
+    val q = ArrayDeque<String>()
+    q.add(root)
+
+    while (q.isNotEmpty()) {
+        ans = ans.coerceAtLeast(q.size)
+
+        repeat(q.size) {
+            val cur = q.removeFirst()
+            graph[cur]?.forEach { child ->
+                q.add(child)
+            }
+        }
+    }
+
+    println(ans)
+}
