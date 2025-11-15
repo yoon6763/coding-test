b9678972e3e5097b86bc28005fc7db5aac7f9dcc
전윤호 / uknow <yoon986763@gmail.com>
2025-11-15 22:34:55 +0900

    25.11.15

---
+++ kotlin_java/src/main/java/bronze/b3/ИЗЛОЖЕНИЕ НА ПЧЕЛЕН МЕД.kt
@@ -0,0 +1,25 @@
+package bronze.b3
+
+fun main() {
+    val prices = readln().split(" ").map { it.toLong() }
+    val arr = readln().split(" ").map { it.toLong() }
+
+    var ans = 0L
+
+    listOf(
+        listOf(0, 1, 2),
+        listOf(0, 2, 1),
+        listOf(1, 0, 2),
+        listOf(1, 2, 0),
+        listOf(2, 0, 1),
+        listOf(2, 1, 0)
+    ).forEach { p ->
+        ans = ans.coerceAtLeast(
+            prices[p[0]] * arr[0] +
+                    prices[p[1]] * arr[1] +
+                    prices[p[2]] * arr[2]
+        )
+    }
+
+    println(ans)
+}
