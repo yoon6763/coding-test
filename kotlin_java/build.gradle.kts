8244b3cb428f4beb7e40fdb0c8948f235714a799
전윤호 / uknow <yoon986763@gmail.com>
2025-11-16 13:57:01 +0900

    25.11.16

---
+++ kotlin_java/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    kotlin("jvm")
+}
+
+group = "baekjoon"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ kotlin_java/src/main/java/bronze/b4/Acquiring SW-IT Corn.kt
@@ -0,0 +1,12 @@
+package bronze.b4
+
+fun main() {
+    val (X, Y, Z) = readln().split(" ").map { it.toLong() }
+    val (U, V, W) = readln().split(" ").map { it.toLong() }
+
+    println(
+        (U / 100) * X +
+                (V / 50) * Y +
+                (W / 20) * Z
+    )
+}
