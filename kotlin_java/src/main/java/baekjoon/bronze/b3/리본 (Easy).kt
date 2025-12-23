87143ad963e7779b1d3699e87b461a815b54f41d
전윤호 / uknow <yoon6763@naver.com>
2025-12-23 20:15:17 +0900

    25.12.23

--- kotlin_java/src/main/java/baekjoon/bronze/b3/리본 (Easy).kt
+++ kotlin_java/src/main/java/baekjoon/bronze/b3/리본 (Easy).kt
@@ -1,5 +1,22 @@
 package baekjoon.bronze.b3
 
-fun main() {
+import kotlin.math.abs
 
+fun main() {
+    val n = readln().toInt()
+    val x = readln().split(" ").map { it.toLong() }
+    val l = readln().split(" ").map { it.toLong() }
+    val c = readln().split(" ")
+
+    for (i in 0 until n) {
+        for (j in i + 1 until n) {
+            if (c[i] != c[j] && abs(x[i] - x[j]) <= l[i] + l[j]) {
+                println("YES")
+                println("${i + 1} ${j + 1}")
+                return
+            }
+        }
+    }
+
+    println("NO")
 }
