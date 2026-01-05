50c4e83c4b30e40d8ea1142820386584bfe70c5a
전윤호 / uknow <yoon6763@naver.com>
2026-01-05 21:43:30 +0900

    26.01.05

---
+++ kotlin_java/src/main/java/baekjoon/bronze/b4/연대 다음 고대.kt
@@ -0,0 +1,14 @@
+package baekjoon.bronze.b4
+
+fun main() {
+    while (true) {
+        val line = readln()
+        if (line == "yonsei") {
+            println("Yonsei Won!")
+            break
+        } else if (line == "korea") {
+            println("Yonsei Lost...")
+            break
+        }
+    }
+}
