6f0a72e198ad9ccbe839d08b35821378dcb23d5d
전윤호 / uknow <yoon6763@naver.com>
2026-01-22 23:52:57 +0900

    26.01.22

---
+++ kotlin_java/src/main/java/baekjoon/bronze/b5/채플이 너무해.kt
@@ -0,0 +1,11 @@
+package baekjoon.bronze.b5
+
+fun main() {
+    val x = readln().toInt()
+
+    if (x + 2 >= 8) {
+        println("Success!")
+    } else {
+        println("Oh My God!")
+    }
+}
