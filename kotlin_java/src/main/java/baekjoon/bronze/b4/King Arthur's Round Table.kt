638a5f297590fd62f05ae9d90eb06bf3b968c211
전윤호 / uknow <yoon6763@naver.com>
2026-02-06 23:55:48 +0900

    26.02.06

---
+++ kotlin_java/src/main/java/baekjoon/bronze/b4/King Arthur's Round Table.kt
@@ -0,0 +1,8 @@
+package baekjoon.bronze.b4
+
+fun main() {
+    if (3.14159 * readln().toDouble() >= readln().toDouble() * readln().toInt())
+        println("YES")
+    else
+        println("NO")
+}
